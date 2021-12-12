e6758c4a5232de1844df6337c622040b8c14ac80
Aligan18 <alisherganiev18@gmail.com>
2021-12-13 00:24:50 +0600

    mongo db added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const MONGO_URL='mongodb://Aligan18:Aligan18@moda-store-shard-00-00.6zpix.mongodb.net:27017,moda-store-shard-00-01.6zpix.mongodb.net:27017,moda-store-shard-00-02.6zpix.mongodb.net:27017/moda-store?ssl=true&replicaSet=atlas-4rv5w7-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+const serverRun =async()=>{
+    try {
+        await mongoose.connect(MONGO_URL)
+        console.log('Mobgodb running')
+    } catch (error) {
+        console.error(error)
+    }
+}
+serverRun()
 
 app.listen(5000, ()=>{
     console.log('SERVER RUNNING IN 5000 PORT')
