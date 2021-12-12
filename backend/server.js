c9f5d64ef34a08af0855cfd894adfcb09b0155fc
Aligan18 <alisherganiev18@gmail.com>
2021-12-13 00:26:56 +0600

    add Port

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const PORT= 5000
 const MONGO_URL='mongodb://Aligan18:Aligan18@moda-store-shard-00-00.6zpix.mongodb.net:27017,moda-store-shard-00-01.6zpix.mongodb.net:27017,moda-store-shard-00-02.6zpix.mongodb.net:27017/moda-store?ssl=true&replicaSet=atlas-4rv5w7-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 const serverRun =async()=>{
